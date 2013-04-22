295e770d0adf9ccc3150d7e4ef49e249ef94a23f
Jan Chmiel <chmiel.jan@gmail.com>
2013-04-22 12:06:34 +0200

    Add creating parent directories if necessary.

--- configure.sh
+++ configure.sh
@@ -10,12 +10,18 @@ create_link() {
     echo "Path $dest_path exists, cannot be linked."
     return
   fi
+  dest_path_dir=$(dirname /home/johnny/.zsh/functions.bash)
+  if [ ! -e "$dest_path_dir" ]; then
+    cmd="mkdir -p $dest_path_dir"
+    echo $cmd
+    eval "$cmd"
+  fi
   cmd="ln -s $src_path $dest_path"
   echo $cmd
   eval "$cmd"
 }
 
-. ./configure2.conf
+. ./configure.conf
 
 for x in ${dotfiles[@]}; do
   create_link "$PWD/$x" "$HOME/.$x"
