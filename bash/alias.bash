ff4e1dd4faa4516def263e09ccd6ecf80ee7ed91
Jan Chmiel <chmiel.jan@gmail.com>
2015-06-30 19:09:25 +0200

    Some new aliases.

--- bash/alias.bash
+++ bash/alias.bash
@@ -2,14 +2,13 @@ alias ll='ls -l --color=auto'
 alias ls='ls --color=auto'
 alias la='ls -la --color=auto'
 
-# Nice ubuntu aliases.
+# Some apt aliases.
 alias aptfs='apt-file search'
 alias apts='apt-cache search'
 alias apti='sudo apt-get install'
 alias aptu='sudo apt-get update'
 alias aptr='sudo apt-get --purge remove'
 
-# Other ones useful too.
 alias grep='grep --color=auto'
 alias gr='grep --color=auto'
 alias pg='ping google.com'
@@ -21,11 +20,21 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
+alias od='popd'
+alias pd='pushd'
+alias v='vim'
+alias vmore="vim -u ~/.vimrc.more -"
+alias cls=clear
+alias gr=grep --color=auto
+alias tf='tail -f'
+alias wg=wget
+alias mp=mplayer
 
+# Some git aliases.
 alias g='git'
 alias gb='git branch'
-alias gsh='git show'
-alias gs='git status'
+alias gs='git show'
+alias gst='git status'
 alias gc='git checkout'
 alias gcb='git checkout -b'
 alias ga='git add'
@@ -33,11 +42,3 @@ alias gci='git commit'
 alias gd='git --no-pager diff'
 alias gl='git --no-pager log --pretty=oneline -n25 --abbrev-commit'
 alias gg='git --no-pager grep -n -H'
-alias vmore="vim -u ~/.vimrc.more -"
-alias od='popd'
-alias pd='pushd'
-alias v='vim'
-alias cls=clear
-alias gr=grep --color=auto
-alias tf='tail -f'
-alias wg=wget
